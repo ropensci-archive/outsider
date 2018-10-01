29095427b233591ccf711af46be1394e4287b179
Dom Bennett <dominic.john.bennett@gmail.com>
2018-10-01 13:27:52 +0200

    minor

--- R/outsider.R
+++ R/outsider.R
@@ -5,3 +5,12 @@
 #' @docType package
 #' @name outsider
 NULL
+
+.run <- function(pkgnm, ...) {
+  ids <- .ids_get(pkgnm = pkgnm)
+  # launch container
+  .docker_start(cntnr_id = ids[['cntnr_id']], img_id = ids[['img_id']])
+  on.exit(.docker_stop(cntnr_id = ids[['cntnr_id']]))
+  # run command
+  invisible(.docker_exec(cntnr_id = ids[['cntnr_id']], ...))
+}
